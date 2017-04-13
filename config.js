48b9ca58ea868ccb50b05934c2d643644557e435
jasondibenedetto <jason.dibenedetto6@gmail.com>
2017-04-14 00:02:39 +0930

    remove secrets.json

--- config.js
+++ config.js
@@ -2,17 +2,17 @@ module.exports = {
 
   // Your bot name. Typically, this is your bot's username without the discriminator.
   // i.e: if your bot's username is MemeBot#0420, then this option would be MemeBot.
-  name: "garybot",
+  name: 'garybot',
 
   // The bot's command prefix. The bot will recognize as command any message that begins with it.
   // i.e: "-garybot foo" will trigger the command "foo",
   //      whereas "garybot foo" will do nothing at all.
-  prefix: "garybot",
+  prefix: 'garybot',
 
   // Your bot's user token. If you don't know what that is, go here:
   // https://discordapp.com/developers/applications/me
   // Then create a new application and grab your token.
-  token: require('./secrets.json').token,
+  token: 'MzAyMDM5OTkwNzY0ODMwNzMw.C9DyZw.8_8BXyZrsrckE_UZtEtR0ADQa4M',
 
   // If this option is enabled, the bot will delete the message that triggered it, and its own
   // response, after the specified amount of time has passed.
@@ -23,5 +23,5 @@ module.exports = {
   deleteAfterReply: {
     enabled: false,
     time: 10000, // In milliseconds
-  }
+  },
 };
